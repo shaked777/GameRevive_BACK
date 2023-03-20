fba963e8d1ce97c2f9eb468e34f26be25ba42756
shaked777 <shaked.g1010@gmail.com>
2023-03-20 23:48:00 +0200

    digital ocean deploy15

--- backend/urls.py
+++ backend/urls.py
@@ -21,6 +21,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('api/products/', include('base.urls.products_urls')),
     path('api/users/', include('base.urls.users_urls')),
     path('api/stripe/', include('payments.urls'))
