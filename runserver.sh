ae49cac00e599c98fac1c1bb59a1f88cd19e9f96
shaked777 <shaked.g1010@gmail.com>
2023-03-23 23:07:39 +0200

    digital ocean deploy24

--- runserver.sh
+++ runserver.sh
@@ -1,6 +1,4 @@
-NUM_WORKERS=3
-TIMEOUT=120
 
 python manage.py migrate
 
-gunicorn --worker-tmp-dir /dev/shm backend.wsgi
+gunicorn --worker-tmp-dir /dev/shm backend.wsgi --timeout 120 --workers=3 --worker-class=gevent --threads=3 --worker-connections=1000 
